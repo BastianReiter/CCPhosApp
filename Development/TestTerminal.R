810f91971ca9e7c52ea4cb6488584499d369dc48
BastianReiter <150595257+BastianReiter@users.noreply.github.com>
2024-04-08 16:13:30 +0200

    Small fix

--- Development/TestTerminal.R
+++ Development/TestTerminal.R
@@ -37,33 +37,3 @@ WorkspaceInfo <- GetServerWorkspaceInfo(DataSources = CCPConnections)
 
 
 
-
-
-
-library(shiny)
-library(datamods)
-library(bslib)
-library(reactable)
-
-ui <- shiny.semantic::semanticPage(
-  tags$h2(i18n("Edit data"), align = "center"),
-  edit_data_ui(id = "id"),
-  verbatimTextOutput("result")
-)
-
-
-server <- function(input, output, session) {
-
-  edited_r <- edit_data_server(
-    id = "id",
-    data_r = reactive(demo_edit)
-  )
-
-  output$result <- renderPrint({
-    str(edited_r())
-  })
-
-}
-
-if (interactive())
-  shinyApp(ui, server)
